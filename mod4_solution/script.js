59245082faf6fb28564f6af8596cb58f4d0d9d75
SIDDAM VINAY <64787261+siddamvinay2001@users.noreply.github.com>
2022-01-19 15:32:02 +0530

    Update script.js

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -33,7 +33,7 @@ WARNING!!! WARNING!!!
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Vinay", "Siddam", "Kavya", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
